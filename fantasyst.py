da71b7377628bd69ba817b3da7c8ad6a69674646
Sebastian <lafamilia@MacBook-Pro-de-La.local>
2026-05-29 11:21:55 -0300

    upload code to suscriptionmode

--- fantasyst.py
+++ fantasyst.py
@@ -2,16 +2,57 @@ import json
 
 def lambda_handler(event, context):
 
-    response = {
-        "user": "sebastian",
-        "subscription_status": "active",
-	"cicd": "working",
-    	"plan": "premium",
-    	"region": "sa-east-1",
-        "version": "1.1.0"
+    users = {
+        "sebastian": {
+            "plan": "premium",
+            "region": "sa-east-1",
+            "status": "active"
+        },
+        "marina": {
+            "plan": "basic",
+            "region": "us-east-1",
+            "status": "active"
+        },
+        "angelica": {
+            "plan": "premium",
+            "region": "us-east-1",
+            "status": "active"
+        },
+        "samuel": {
+            "plan": "basic",
+            "region": "sa-east-1",
+            "status": "active"
+        },
+        "miguel": {
+            "plan": "basic",
+            "region": "us-west-1",
+            "status": "inactive"
+        },
+        "northon": {
+            "plan": "premium",
+            "region": "us-east-2",
+            "status": "active"
+        },
+        "sergio": {
+            "plan": "premium",
+            "region": "us-east-1",
+            "status": "inactive"
+        }
     }
+
+    user = event.get("queryStringParameters", {}).get("user", "sebastian")
+
+    response = users.get(
+        user,
+        {
+            "error": "user not found"
+        }
+    )
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
         "body": json.dumps(response)
     }
