e7577258f97e96c962e951599192f8ce82ec9ad3
Sebastian <lafamilia@MacBook-Pro-de-La.local>
2026-05-29 09:07:14 -0300

    rename subscription field

--- fantasyst.py
+++ fantasyst.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
 
     response = {
         "user": "sebastian",
-        "subscription": "active"
+        "subscription_status": "active"
     }
 
     return {
